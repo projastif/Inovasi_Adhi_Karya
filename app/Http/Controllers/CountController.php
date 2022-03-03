01cffd30180c86c7e861bc90e19254a65c684c12
BosToken <faizdiandra@gmail.com>
2022-03-03 17:13:57 +0700

    SudutPerhitunganSudutAlatUkur

--- app/Http/Controllers/CountController.php
+++ app/Http/Controllers/CountController.php
@@ -22,25 +22,32 @@ class CountController extends Controller
         $d1 = $sin / 360;
         $d2 = $d1 % 2;
         $derajat = floor($d2);
-        $m1 = ($sin * 60) / 60;
-        $m2 = $m1 % 2;
-        $meter = round($m2);
-        $s1 = $sin * 3600 / 60;
-        $second = $s1 % 2;
-        $decimal = 90 / $sin;
+        // $m1 = ($sin * 60);
+        // $m2 = $m1 % 2;
+        $meter = ($sin * 60) % 60;
+        // $s1 = $sin * 3600 / 60;
+        // $second = $s1 % 2;
+        $second = ($sin * 3600) % 60;
+        $decimal = 90 - $sin;
+        // $m = ($sin * 60) / 60;
 
         //Sudut Alat Ukur//
         $d1b = $decimal/360;
         $d2b = $d1b % 2;
-        $derajat2 = floor($d2b);
+        // $derajat2 = floor($d2b);
+        $derajat2 = $decimal % 360;
         $m1b = ($decimal * 60) / 60;
         $m2b = $m1b % 2;
-        $meter2 = round($m2b);
+        // $meter2 = round($m2b);
+        // $meter2 = ($sin * 60) / 60;
+        $meter2 = ($decimal * 60) % 60;
         $s1b = $decimal * 3600 / 60;
-        $second2 = $s1b % 2;
+        // $second2 = $s1b % 2;
+        $second2 = ($decimal * 3600) % 60;
 
         //return redirect('/')->with('info','Hasil Dari '.$lokasipertama.' - '.$lokasikedua.' adalah : '.$hasil);
         return redirect('/')->with('info','Lokasi Pertama : ' .$lokasipertama.', Elevasi Pertama : ' .$elevasipertama. ', Lokasi Kedua : ' .$lokasikedua. ', Elevasi Kedua : ' .$elevasikedua. ', Beda Tinggi : ' .$bedatinggi. ', Jarak : ' .$jarak. ', i(%) : ' .$i. ', Δ sin α : ' .$sin. ', D° : ' .$derajat. ', M : ' .$meter. ', S : ' .$second. ', Decimal : ' .$decimal. ', D° : ' .$derajat2. ', M : ' .$meter2. ', S : ' .$second2);
+        // return ceil(($sin * 60) % 60);
     }
 
     public function sudut_perhitungan(Request $request){
